7684e6724261cede5fa5af4daa1382e2797b319f
Romain Ouabdelkader <romain.ouabdelkader@gmail.com>
2019-09-10 00:09:18 +0200

    cleanup

--- caribou/cli.py
+++ caribou/cli.py
@@ -5,8 +5,7 @@ from caribou.ui import run
 def main():
     if len(sys.argv) != 2:
         print('Usage: caribou <file.py>')
-        path = '/Users/ouabde_r/signals/server/docs/routes.py'
-        # exit(1)
+        exit(1)
     else:
         path = sys.argv[1]
     run(path)
